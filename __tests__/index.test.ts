8a5854dae20460c3e06d198ad3aa2a5fb0af285d
Abir <abir.pal899@gmail.com>
2022-08-16 23:02:34 +0530

    test: add test suite for class method testing and tests for class methods

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,28 +3,74 @@ import ProblemContextHelper from "./_helper";
 
 const _testContext = new ProblemContextHelper();
 
-test("create problem with custom keys", () => {
-  const customKey = "RCA";
-  const testProblem = new Problem(_testContext._problemInstance, ["RCA"]);
-  expect(testProblem).toHaveProperty(customKey);
-});
+describe("Class Methods Test Suite", () => {
+  test("Create Class with Custom Keys", () => {
+    const customKey = "RCA";
+    const testProblem = new Problem(_testContext._problemInstance, ["RCA"]);
+    expect(testProblem).toHaveProperty(customKey);
+  });
 
-test('copy problem with mode="LEAVE PROPS"', () => {
-  const _problemCopy = _testContext._problemInstance.copy(
-    _testContext._problemInstance,
-    COPY_MODE.LEAVE_PROPS,
-    []
-  );
-  expect(_problemCopy).toBeInstanceOf(Problem);
-  expect(_problemCopy.type).toBe(_testContext._problemInstance.type);
-  expect(_problemCopy.title).toBe(_testContext._problemInstance.title);
-});
+  test("Method: Copy, mode: LEAVE_PROPS", () => {
+    const _problemCopy = _testContext._problemInstance.copy(
+      _testContext._problemInstance,
+      COPY_MODE.LEAVE_PROPS,
+      []
+    );
+    expect(_problemCopy).toBeInstanceOf(Problem);
+    expect(_problemCopy.type).toBe(_testContext._problemInstance.type);
+    expect(_problemCopy.title).toBe(_testContext._problemInstance.title);
+  });
 
-test('copy problem with mode="SKIP PROPS"', () => {
-  const _copiedProblem = _testContext._problemInstance.copy(
-    _testContext._problemInstance,
-    COPY_MODE.SKIP_PROPS,
-    ["details"]
-  );
-  expect(_copiedProblem.details).toBeUndefined();
+  test("Method: Copy, mode: SKIP_PROPS", () => {
+    const _copiedProblem = _testContext._problemInstance.copy(
+      _testContext._problemInstance,
+      COPY_MODE.SKIP_PROPS,
+      ["details"]
+    );
+    expect(_copiedProblem.details).toBeUndefined();
+  });
+
+  test("Method: isOfType", () => {
+    const _TYPE_TRUTH_CHECK = "null-or-falsey-document";
+    const _TYPE_FALSE_CHECK = "undefined-document";
+    expect(
+      _testContext._problemInstance.isOfType(_TYPE_TRUTH_CHECK)
+    ).toBeTruthy();
+    expect(
+      _testContext._problemInstance.isOfType(_TYPE_FALSE_CHECK)
+    ).toBeFalsy();
+  });
+
+  test("Method: Update", async () => {
+    const LINK: string = "test-link/";
+    const updates = {
+      link: LINK,
+    };
+    await _testContext._problemInstance.update({ updates });
+    expect(_testContext._problemInstance).toHaveProperty("link", LINK);
+  });
+
+  test("Method: toJSON", () => {
+    const _problem = _testContext._problemInstance;
+
+    // Run updates first to add stack property, required for testing  isJSON
+    const updates = {
+      stack: "./filepath",
+    };
+    _problem.update({ updates });
+
+    // non-stringified stack
+    expect(_problem.toJSON({ includeStack: true })).toHaveProperty(
+      "stack",
+      updates.stack
+    );
+    expect(_problem.toJSON({ includeStack: false })).not.toHaveProperty(
+      "stack"
+    );
+
+    // stringified stack
+    expect(_problem.toJSON({ includeStack: true, stringify: true })).toBe(
+      JSON.stringify({ ..._problem })
+    );
+  });
 });
