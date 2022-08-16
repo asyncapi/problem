de82c4fdb34be9bb6042eeebe637d925e02d382c
Abir <abir.pal899@gmail.com>
2022-08-16 23:02:07 +0530

    feat:add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,25 @@
-export type ProblemInterface = {
-    type: string,
-    title: string,          // Title should be description of http status, if type is not present.
-    http?: httpObject,
-    detail?: string,
-    instance?: string,      // Details to reproduce the error.
-    stack?: string;
-    [key: string]: any,      // Custom Field of Problem
-}
+import { Problem } from "problem";
 
-export type httpObject= {
-    status: number,          // Status Code
-    [key: string]: any,
+export type ProblemInterface = {
+  type: string;
+  title: string; // Title should be description of http status, if type is not present.
+  http?: httpObject;
+  detail?: string;
+  instance?: string; // Details to reproduce the error.
+  stack?: string;
+  [key: string]: any; // Custom Field of Problem
+};
+
+export type httpObject = {
+  status: number; // Status Code
+  [key: string]: any;
+};
+
+export type UpdateProblemParamType = {
+  updates: { [key: string]: any };
+};
+
+export type ToJsonParamType = {
+  includeStack?:boolean
+  stringify?:boolean
 }
